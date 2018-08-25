6d9afe7eef6bf5bfc02d2a7223fea599fbe2a5eb
relief.melone <christian.debiasi@hotmail.de>
2018-08-25 12:13:19 +0200

    fully functional

--- src/services/services.errors.js
+++ src/services/services.errors.js
@@ -1,14 +1,15 @@
 function TimeoutError(Obj){
-    this.Obj = Obj;
-    this.TimedoutAfterMillis = new Date() - Obj.started;
+    this.obj = Obj;
+    this.timedoutAfterMillis = new Date() - Obj.started;
     this.code = 408 
     return this;
 }
 
 function RetryRejectError(Obj, Err){
-    this.Obj = Obj;
-    this.FailedAfterMillis = new Date() - Obj.started;
+    this.obj = Obj;
+    this.failedAfterMillis = new Date() - Obj.started;
     this.code = Err.code || null;
+    this.err = Err;
     return this;
 }
 
